3ee2c9e1525e42584581d337d477bcce802d13b6
Sarvesh <10650sarvesh@gmail.com>
2026-03-14 22:27:27 +0530

    empty

--- components/NotesClient.jsx
+++ components/NotesClient.jsx
@@ -25,7 +25,7 @@ function NotesClient() {
   return (
     <div className="min-h-screen w-screen bg-black flex flex-col p-4 pt-8 lg:items-center">
       <Toaster position="bottom-right" />
-      <h1 className="text-white font-bold text-4xl ml-8 lg:ml-0">NotesNext</h1>
+      <h1 className="text-white font-bold text-4xl ml-8 lg:ml-0">Notes Next</h1>
       <NewNoteButton adding={adding} setAdding={setAdding} />
 
       <NewNoteForm adding={adding} setAdding={setAdding} setNotes={setNotes} />
